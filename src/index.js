9333b32abd59ede21ab767e662dd6b8c66126203
Thor Anderson <ta.anderson@gmail.com>
2018-10-30 14:49:29 -0600

    Initial working

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('Hello World!')
+import 'dotenv/config'
+import fs from 'fs'
+
+const content = fs.readFileSync("./uvu_courses.json");
+const catalog = JSON.parse(content)
+
+const allCourses = catalog.comet.course
+
+const result = allCourses.filter(course => course.prefix._text === 'DGM');
+
+console.log(result.length)
