a75678280167cf58f009d4272c8ad2a7fbf08720
Thor Anderson <ta.anderson@gmail.com>
2018-10-30 20:06:34 -0600

    Getting close

--- src/index.js
+++ src/index.js
@@ -1,11 +1,102 @@
 import 'dotenv/config'
 import fs from 'fs'
 
+import { GraphQLClient } from 'graphql-request'
+
+const client = new GraphQLClient('http://localhost:4466')
+
 const content = fs.readFileSync("./uvu_courses.json");
 const catalog = JSON.parse(content)
 
 const allCourses = catalog.comet.course
 
-const result = allCourses.filter(course => course.prefix._text === 'DGM');
+const dgmCourses = allCourses.filter(course => course.prefix._text === 'DGM');
+/* 
+console.log(dgmCourses.map(crs => {
+    return {
+        prefix: crs.prefix._text || '',
+        subject: crs.subject._text || '',
+        number: crs.number._text || '',
+        crossListed: crs.crossListed._text || '',
+        geCode: crs.geCode._text || '',
+        title: crs.title._text || '',
+        totalCredits: crs.totalCredits._text || '',
+        lectureCredits: crs.lectureCredits._text || '',
+        termsOffered: crs.termsOffered._text || '',
+        prereq: crs.prereq._text || '',
+        coreq: crs.coreq._text || '',
+        preorco: crs.preorco._text || '',
+        description: crs.description._text || ''
+    }
+})) */
 
-console.log(result.length)
+const mutation = `mutation createCourse(
+    $owner: UserCreateOneInput!,
+    $prefix: String,
+    $subject: String,
+    $number: String,
+    $crossListed: String,
+    $geCode: String,
+    $title: String!,
+    $totalCredits: String,
+    $lectureCredits: String,
+    $termsOffered: String,
+    $prereq: String,
+    $coreq: String,
+    $preorco: String,
+    $description: String,
+    ) {
+        createCourse(
+            data: {
+                owner: $owner
+                prefix: $prefix
+                subject: $subject
+                number: $number
+                crossListed: $crossListed
+                geCode: $geCode
+                title: $title
+                totalCredits: $totalCredits
+                lectureCredits: $lectureCredits
+                termsOffered: $termsOffered
+                prereq: $prereq
+                coreq: $coreq
+                preorco: $preorco
+                description: $description
+            }
+        )
+        {
+            id
+            title
+        }
+    }`
+
+async function main() {
+    for (let crs of dgmCourses) {
+        const ownerEmail = 'admin@uvu.edu'
+        let variables = {
+            owner: {
+                connect: {
+                  email: ownerEmail
+                }
+              },
+            prefix: crs.prefix._text || '',
+            subject: crs.subject._text || '',
+            number: crs.number._text || '',
+            crossListed: crs.crossListed._text || '',
+            geCode: crs.geCode._text || '',
+            title: crs.title._text || '',
+            totalCredits: crs.totalCredits._text || '',
+            lectureCredits: crs.lectureCredits._text || '',
+            termsOffered: crs.termsOffered._text || '',
+            prereq: crs.prereq._text || '',
+            coreq: crs.coreq._text || '',
+            preorco: crs.preorco._text || '',
+            description: crs.description._text || ''
+        }
+    }
+    await client
+        .request(mutation, variables)
+        .then(data => console.log(data))
+        .catch(err => console.log(`${err} ${count}`))
+}
+main()
